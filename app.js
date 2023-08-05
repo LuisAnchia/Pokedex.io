38a0c62cba9d6e19d9e833a81caafcf4ffb65d37
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-08-05 12:50:58 -0600

    up
    up

--- app.js
+++ app.js
@@ -1,67 +1,101 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const { MongoClient, ObjectId } = require('mongodb');
 
-const uri = 'mongodb+srv://anchiaprogram1:hw85jSFpdTgDPW1J@cluster0.tdratzf.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
-let cardsCollection;
+const app = express();
+const port = process.env.PORT || 3000;
 
+const uri = 'mongodb+srv://anchiaprogram1:hw85jSFpdTgDPW1J@cluster0.tdratzf.mongodb.net/Pikachu?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Configuración para procesar JSON en las solicitudes
-app.use(express.json());
-// Configuración para servir archivos estáticos desde la carpeta public
-app.use(express.static('public'));
+let pokemonsCollection;
 
-const pokemons = [];
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    const db = client.db('Pikachu');
+    pokemonsCollection = db.collection('pokemones');
+    console.log('Connected to MongoDB successfully!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
 
-// Registro de pokémones
-app.post('/pokemon', (req, res) => {
+connectToDatabase();
+
+// Rutas del API
+
+// Ruta para registrar un nuevo Pokémon
+app.post('/api/pokemons', express.json(), async (req, res) => {
   const newPokemon = req.body;
-  newPokemon.id = Date.now().toString();
-  pokemons.push(newPokemon);
-  res.status(201).json(newPokemon);
-});
-
-// Visualización de todos los pokémons
-app.get('/pokemon', (req, res) => {
-  res.json(pokemons);
-});
-
-// Búsqueda de pokémons
-app.get('/pokemon/search', (req, res) => {
-  const searchTerm = req.query.q.toLowerCase();
-  const filteredPokemons = pokemons.filter(pokemon => {
-    return pokemon.name.toLowerCase().includes(searchTerm) || pokemon.type.toLowerCase().includes(searchTerm);
-  });
-  res.json(filteredPokemons);
-});
-
-// Edición de pokémons
-app.put('/pokemon/:id', (req, res) => {
-  const id = req.params.id;
-  const updatedPokemon = req.body;
-  const index = pokemons.findIndex(pokemon => pokemon.id === id);
-  if (index !== -1) {
-    pokemons[index] = updatedPokemon;
-    res.json(updatedPokemon);
-  } else {
-    res.status(404).json({ message: 'Pokémon not found' });
+  try {
+    const result = await pokemonsCollection.insertOne(newPokemon);
+    res.status(201).json(result.ops[0]);
+  } catch (error) {
+    console.error('Error creating a new Pokémon:', error);
+    res.status(500).json({ error: 'Failed to create a new Pokémon' });
   }
 });
 
-// Eliminación de pokémons
-app.delete('/pokemon/:id', (req, res) => {
-  const id = req.params.id;
-  const index = pokemons.findIndex(pokemon => pokemon.id === id);
-  if (index !== -1) {
-    const deletedPokemon = pokemons.splice(index, 1);
-    res.json(deletedPokemon[0]);
-  } else {
-    res.status(404).json({ message: 'Pokémon not found' });
+// Ruta para obtener todos los Pokémons
+app.get('/api/pokemons', async (req, res) => {
+  try {
+    const pokemons = await pokemonsCollection.find({}).toArray();
+    res.json(pokemons);
+  } catch (error) {
+    console.error('Error getting all Pokémons:', error);
+    res.status(500).json({ error: 'Failed to get all Pokémons' });
+  }
+});
+
+// Ruta para obtener un Pokémon por su ID
+app.get('/api/pokemons/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const pokemon = await pokemonsCollection.findOne({ _id: ObjectId(id) });
+    if (!pokemon) {
+      return res.status(404).json({ message: 'Pokémon not found' });
+    }
+    res.json(pokemon);
+  } catch (error) {
+    console.error('Error getting a Pokémon by ID:', error);
+    res.status(500).json({ error: 'Failed to get a Pokémon' });
+  }
+});
+
+// Ruta para actualizar un Pokémon por su ID
+app.put('/api/pokemons/:id', express.json(), async (req, res) => {
+  const { id } = req.params;
+  const updatedPokemon = req.body;
+  try {
+    const result = await pokemonsCollection.updateOne(
+      { _id: ObjectId(id) },
+      { $set: updatedPokemon }
+    );
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: 'Pokémon not found' });
+    }
+    res.json({ message: 'Pokémon updated successfully' });
+  } catch (error) {
+    console.error('Error updating a Pokémon:', error);
+    res.status(500).json({ error: 'Failed to update a Pokémon' });
+  }
+});
+
+// Ruta para eliminar un Pokémon por su ID
+app.delete('/api/pokemons/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pokemonsCollection.deleteOne({ _id: ObjectId(id) });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'Pokémon not found' });
+    }
+    res.json({ message: 'Pokémon deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting a Pokémon:', error);
+    res.status(500).json({ error: 'Failed to delete a Pokémon' });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
